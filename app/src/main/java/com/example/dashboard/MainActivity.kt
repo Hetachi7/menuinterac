aa4218e800f3f6caad458721a3301162205f8b80
axelcastr <114270665+axelcastr@users.noreply.github.com>
2023-09-20 14:58:21 -0500

    segunndo paso

--- app/src/main/java/com/example/dashboard/MainActivity.kt
+++ app/src/main/java/com/example/dashboard/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dashboard
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 
@@ -14,10 +15,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, clothing::class.java)
             startActivity(intent)
         }
-        val electronicCard = findViewById<CardView>(R.id.elecCard)
-        electronicCard.setOnClickListener {
-            val intent = Intent(this@MainActivity, Electronics::class.java)
-            startActivity(intent)
+        val electroniCard = findViewById<CardView>(R.id.elecCard)
+        electroniCard.setOnClickListener {
+            val intent2 = Intent(this@MainActivity, Electronics::class.java)
+            startActivity(intent2)
+            val toast = Toast.makeText(this, "Electronics", Toast.LENGTH_LONG)
+            toast.show()
+        }
+        val HomeCard = findViewById<CardView>(R.id.Homecard)
+        HomeCard.setOnClickListener {
+            val intent3 = Intent(this@MainActivity, Home::class.java)
+            startActivity(intent3)
+        }
+        val BeautyCard = findViewById<CardView>(R.id.Beatycard2)
+        BeautyCard.setOnClickListener {
+            val intent4 = Intent(this@MainActivity, Beauty::class.java)
+            startActivity(intent4)
+        }
+        val phamarCard = findViewById<CardView>(R.id.phamarcyCard)
+        phamarCard.setOnClickListener {
+            val intent5 = Intent(this@MainActivity, Phamarcy::class.java)
+            startActivity(intent5)
+        }
+        val groCard = findViewById<CardView>(R.id.groceriesCard)
+        groCard.setOnClickListener {
+            val intent6 = Intent(this@MainActivity, groceries::class.java)
+            startActivity(intent6)
         }
     }
 }
